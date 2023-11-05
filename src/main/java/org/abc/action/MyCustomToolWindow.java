26966c9b7240dc764284a6b995451a0f0fd59ee2
henry wang <421053122@qq.com>
2023-11-05 22:46:25 +0800

    revise the toolwindow ui

--- src/main/java/org/abc/action/MyCustomToolWindow.java
+++ src/main/java/org/abc/action/MyCustomToolWindow.java
@@ -2,34 +2,44 @@ package org.abc.action;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
+import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.components.JBScrollPane;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 
 public class MyCustomToolWindow {
 
 
-    private final JTextArea textArea = new JTextArea(10,80);
+    private final JTextArea textArea = new JTextArea(20,80);
+    private final JTextField textField = new JTextField();
     
     
     
     
     public SimpleToolWindowPanel createToolWindowContent(final Project project) {
         SimpleToolWindowPanel panel = new SimpleToolWindowPanel(true, true);
-        panel.setBackground(Color.YELLOW);
-        panel.setLayout(new BorderLayout(200, 200));
-        panel.setBorder(BorderFactory.createEmptyBorder(40, 0, 0, 0));
+//        panel.setBackground(Color.YELLOW);
+//        panel.setLayout(new BorderLayout(200, 200));
+//        panel.setBorder(BorderFactory.createEmptyBorder(40, 0, 0, 0));
 
-        textArea.append("setIconLabel(currentDate, CALENDAR_ICON_PATH);\n" +
-                "            setIconLabel(timeZone, TIME_ZONE_ICON_PATH);\n" +
-                "            setIconLabel(currentTime, TIME_ICON_PATH);\n" +
-                "            calendarPanel.add(currentDate);\n" +
-                "            calendarPanel.add(timeZone);\n" +
-                "            calendarPanel.add(currentTime);");
+        textArea.append("This is default text;\n" +
+                "            This is default text;\n" +
+                "            This is default text;\n" +
+                "            This is default text;\n" +
+                "            This is default text;\n" +
+                "            This is default text;");
         textArea.setEditable(false);
         textArea.setLineWrap(true);
-        textArea.setBackground(Color.BLUE);
+//        textArea.setBackground(Color.BLUE);
+        textArea.setFont(new Font("Calibri", Font.PLAIN, 15));
 
 
         JBScrollPane scrollPane = new JBScrollPane(textArea);
@@ -38,7 +48,10 @@ public class MyCustomToolWindow {
         scrollPane.setAutoscrolls(true);
 
 
-        panel.setContent(scrollPane);
+//        panel.setContent(scrollPane);
+        panel.add(scrollPane,BorderLayout.PAGE_START);
+        panel.add(createControlsPanel(), BorderLayout.CENTER);
+
 
         return panel;
 
@@ -46,6 +59,54 @@ public class MyCustomToolWindow {
 
     public void displayMessage(String message) {
         textArea.setText(message);
+    }
+
+    private JPanel createControlsPanel() {
+        JPanel controlsPanel = new JPanel();
+//        controlsPanel.setBackground(Color.RED);
+        JTextPane cityNamePane = new JTextPane();
+        cityNamePane.setText("City: ");
+        controlsPanel.add(cityNamePane);
+
+        textField.setColumns(10);
+        textField.setName("City:");
+        controlsPanel.add(textField);
+
+
+        JButton actionButton = new JButton("Ask ChatGPT");
+        actionButton.addActionListener(e -> {
+            try {
+                cityWeather();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        controlsPanel.add(actionButton);
+//        JButton hideToolWindowButton = new JButton("Hide");
+//        hideToolWindowButton.addActionListener(e -> toolWindow.hide(null));
+//        controlsPanel.add(hideToolWindowButton);
+        return controlsPanel;
+    }
+
+    private void cityWeather() throws IOException {
+
+        HttpClient httpClient = HttpClients.createDefault();
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + textField.getText() + "&appid=6e70a2c441e22fce5a882fe7eb170e9c";
+        HttpGet httpGet = new HttpGet(url);
+
+        HttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        HttpEntity responseEntity = response.getEntity();
+        String responseString = EntityUtils.toString(responseEntity);
+
+        textArea.setText(responseString);
+
+
     }
 
 
--- src/main/java/org/abc/action/PopupDialogAction.java
+++ src/main/java/org/abc/action/PopupDialogAction.java
@@ -1,16 +1,25 @@
 package org.abc.action;
 
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.lang.Language;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.pom.Navigatable;
+import com.intellij.psi.PsiFile;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.io.IOException;
 
 /**
  * Action class to demonstrate how to interact with the IntelliJ Platform.
@@ -51,20 +60,47 @@ public class PopupDialogAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
         // Using the event, create and show a dialog
-        Project currentProject = event.getProject();
-        StringBuilder message =
-                new StringBuilder(event.getPresentation().getText() + " Selected!");
-        // If an element is selected in the editor, add info about it.
-        Navigatable selectedElement = event.getData(CommonDataKeys.NAVIGATABLE);
-        if (selectedElement != null) {
-            message.append("\nSelected Element: ").append(selectedElement);
+//        Project currentProject = event.getProject();
+//        StringBuilder message =
+//                new StringBuilder(event.getPresentation().getText() + " Selected!");
+//        // If an element is selected in the editor, add info about it.
+//        Navigatable selectedElement = event.getData(CommonDataKeys.NAVIGATABLE);
+//        if (selectedElement != null) {
+//            message.append("\nSelected Element: ").append(selectedElement);
+//        }
+//        String title = event.getPresentation().getDescription();
+//        Messages.showMessageDialog(
+//                currentProject,
+//                message.toString(),
+//                title,
+//                Messages.getInformationIcon());
+
+
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://api.openweathermap.org/data/2.5/weather?q=Xi'an&appid=6e70a2c441e22fce5a882fe7eb170e9c");
+
+        HttpResponse response = null;
+        try {
+            response = httpClient.execute(httpGet);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
-        String title = event.getPresentation().getDescription();
-        Messages.showMessageDialog(
-                currentProject,
-                message.toString(),
-                title,
-                Messages.getInformationIcon());
+
+
+        HttpEntity responseEntity = response.getEntity();
+        try {
+            String responseString = EntityUtils.toString(responseEntity);
+            Project project = event.getData(PlatformDataKeys.PROJECT);
+            MyCustomToolWindowFactory.ProjectService projectService = ServiceManager.getService(project, MyCustomToolWindowFactory.ProjectService.class);
+            MyCustomToolWindow toolWindow = projectService.getMyCustomToolWindow();
+            toolWindow.displayMessage(responseString);
+
+
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+
     }
 
     @Override
